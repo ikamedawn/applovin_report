589f142efee84b354bc095b2b6988f7e146db5ac
Dawn <minhpc@ikameglobal.com>
2023-05-25 08:46:56 +0700

    update setup script

--- setup_repo.sh
+++ setup_repo.sh
@@ -18,11 +18,4 @@
 # gh secret set PYPI_API_TOKEN --body $PYPI_API_TOKEN
 # gh secret set TEST_PYPI_API_TOKEN --body $TEST_PYPI_API_TOKEN
 
-# uncomment the following if you need to setup email notification
-# gh secret set BUILD_NOTIFY_MAIL_SERVER --body $BUILD_NOTIFY_MAIL_SERVER
-# gh secret set BUILD_NOTIFY_MAIL_PORT --body $BUILD_NOTIFY_MAIL_PORT
-# gh secret set BUILD_NOTIFY_MAIL_FROM --body $BUILD_NOTIFY_MAIL_FROM
-# gh secret set BUILD_NOTIFY_MAIL_PASSWORD --body $BUILD_NOTIFY_MAIL_PASSWORD
-# gh secret set BUILD_NOTIFY_MAIL_RCPT --body $BUILD_NOTIFY_MAIL_RCPT
-
 # git push -u origin main
